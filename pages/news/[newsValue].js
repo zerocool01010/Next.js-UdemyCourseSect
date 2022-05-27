4d8833d6212ed34c165a9c5a981f644a5ae3b47f
manuelalvarez93 <mrmanuelalvarez@gmail.com>
2022-05-27 16:56:01 -0300

    Agregamos el Link router hook para trabajar de manera mas reactiva y no todo server side rendering

--- pages/news/[newsValue].js
+++ pages/news/[newsValue].js
@@ -9,7 +9,7 @@ function DetailPage() {
 
   return (
     <>
-      <h1>The detail</h1>
+      <h1>The perfection its in the details: {newsV}</h1>
     </>
   );
 }
--- pages/news/index.js
+++ pages/news/index.js
@@ -1,4 +1,5 @@
 /* path: localhost:X/news  */ //toma el nombre de la carpeta como argumento de url y se dirige al index para renderizar
+import Link from 'next/link'
 
 function NewsPage(){
     return <>
@@ -6,7 +7,7 @@ function NewsPage(){
         The news
     </h1>
     <ul>
-        <li><a href="/news/next-js-itsmylife-getitdone">Next.js, what a great thing to be alive</a></li>
+        <li><Link href="/news/next-js-itsmylife-getitdone">Next.js, what a great thing to be alive</Link></li> {/* con Link evitamos mandar una request al server y volvemos a la reactividad que caracteriza React sin hacer todo server side rendering*/}
         <li>Tomate un te</li>
     </ul>
     </>
