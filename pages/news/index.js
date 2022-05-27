1be69beefc247acec39df2a16698931b92dc6b21
manuelalvarez93 <mrmanuelalvarez@gmail.com>
2022-05-27 13:36:27 -0300

    Comenzado el proyecto de Next.js

---
+++ pages/news/index.js
@@ -0,0 +1,11 @@
+/* path: localhost:X/news  */ //toma el nombre de la carpeta como argumento de url y se dirige al index para renderizar
+
+function NewsPage(){
+    return <>
+    <h1>
+        The news
+    </h1>
+    </>
+}
+
+export default NewsPage
---
+++ pages/news/something-important.js
@@ -0,0 +1,11 @@
+/* path: localhost:X/news/something-important  */
+
+function DetailPage(){
+    return <>
+    <h1>
+        The detail
+    </h1>
+    </>
+}
+
+export default DetailPage
