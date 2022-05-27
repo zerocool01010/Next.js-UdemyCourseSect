2f9e3a4700cae0ced982da0b982740dc89a605b3
manuelalvarez93 <mrmanuelalvarez@gmail.com>
2022-05-27 17:59:30 -0300

    usamos el comp MeetUpList para el index

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,36 @@
+import MeetupList from "../components/meetups/MeetupList";
+import React, { useState } from "react";
+
+const dummy_data = [
+    {
+        id: '01',
+        image: 'image',
+        title: 'someimgtitle',
+        address: 'someimgadr'
+    },
+    {
+        id: '01',
+        image: 'image2',
+        title: 'someimgtitle2',
+        address: 'someimgadr2'
+    }
+]
 
 const HomePage = () => {
-    return <>
-    <h1>Home Page</h1>
+    const [] = useState(false)
+  let timer = setTimeout(() => {
+    return (
+      <>
+        <MeetupList />
+      </>
+    );
+  }, 2000);
+  return (
+    <>
+      <h1>Home Page</h1>
+      <MeetupList meetups={dummy_data}/>
     </>
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
