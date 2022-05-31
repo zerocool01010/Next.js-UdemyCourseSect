53d78bb56f1840e78a09f73fa1ef9c313d3ade82
manuelalvarez93 <mrmanuelalvarez@gmail.com>
2022-05-31 13:09:07 -0300

    mas conexiones async con la db de Mongo trayendo datos, ids, objectIds, y renderizando la data que llega y luego cerrando la conexion con .close(). Video 345 incluido del curso

--- pages/[meetupid]/index.js
+++ pages/[meetupid]/index.js
@@ -1,54 +1,57 @@
 import MeetUpDetail from "../../components/meetups/MeetupDetail";
+import { MongoClient, ObjectId } from "mongodb";
 
-function MeetUpDetails() {
+function MeetUpDetails(props) {
   return (
     <MeetUpDetail
-      image="https://upload.wikimedia.org/wikipedia/commons/8/8b/Mexico.Pue.Cholula.Pyramid.01.jpg"
-      title="Cholula Pyramid"
-      address="Pancho Roza, 233, CD"
-      description="A great aztec pyramid in the Center Mexico"
+      image={props.meetupData.image}
+      title={props.meetupData.title}
+      address={props.meetupData.address}
+      description={props.meetupData.description}
     />
   );
 }
 
 export async function getStaticPaths(){ //con esto solucionamso el error generado por no tener el id en un pre-rendering scenario
+    const client = await MongoClient.connect('mongodburlfdsfdafadfafadfadfadfadfadf') 
+    const db = client.db()
+
+    const meetupsCollection = db.collection('meetups')
+
+    const meetups = await meetupsCollection.find({}, {_id: 1}).toArray(); //puse el 1er argumento vacio para que no filtre, y me de todos los elems. En el 2do argumento le digo que solo quiero los ids
+    client.close();
+
     return {
         fallback: false, //false para que solo acepte las opciones debajo definidas, con true se vuelve mas dinamico aunque pasemos un valor no definido debajo
-        paths: [
-            {
-                params: {
-                    meetupid: '01',
-                }
-            },
-            {
-                params: {
-                    meetupid: '02',
-                }
-            },
-            {
-                params: {
-                    meetupid: '03',
-                }
-            }
-        ] //estando el fallback en false por ejemplo no soportara que el usuario ingrese un id 'm4' como parametro
+        paths: meetups.map(
+            (m) => ({params: { meetupId: m._id.toString()}})
+        )
     }
 }
 
 export async function getStaticProps(context) { //fetch data for a single meetup
 
     const meetupId = context.params.meetupid; //no hay que olvidarse que como esta funcion es pre-rendering, entonces es antes de que se monten los componentes, y entonces como consigue el id?
-    console.log(meetupId);
+    
+    const client = await MongoClient.connect('mongodburlfdsfdafadfafadfadfadfadfadf') 
+    const db = client.db()
+
+    const meetupsCollection = db.collection('meetups');
+
+    const selectedMeetup = await meetupsCollection.findOne({_id: ObjectId(meetupId)});
+
+    client.close();
 
     return {
       props: {
         meetupData: {
-            image: 'https://upload.wikimedia.org/wikipedia/commons/8/8b/Mexico.Pue.Cholula.Pyramid.01.jpg',
-            id: meetupId,
-            title: 'First Meetup',
-            address: 'Some Street 5, Some City',
-            description: 'This is a first meetup'
-        },
-      },
+            id: selectedMeetup._id.toString(),
+            title: selectedMeetup.title,
+            address: selectedMeetup.address,
+            image: selectedMeetup.image,
+            description: selectedMeetup.description
+        }
+      }
     };
   }
 
